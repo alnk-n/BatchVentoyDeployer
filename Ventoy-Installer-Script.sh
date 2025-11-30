97675e7766dd83f10291d1c2d0a15cb0c50d0516
alnk-n <alnnagorny@gmail.com>
2025-11-30 15:15:28 +0100

    feat: :sparkles: replace cpz with rsync
    replace cpz with rsync + appropriate flags, as cpz has negligeble benefits when copying to a USB pendrive
    
    Closes #1

--- Ventoy-Installer-Script.sh
+++ Ventoy-Installer-Script.sh
@@ -91,7 +91,7 @@ for choice in $choices; do
   printf "Copying ISO files from $iso_src to $ventoy_mnt ...\n"
   
   # try to copy all ISO files from source folder to mounted partition
-  sudo cpz "$iso_src/"*.iso "$ventoy_mnt"/ || {
+  rsync "$iso_src/"*.iso "$ventoy_mnt"/ -v -h --progress || {
     echo "Failed to copy ISO files to $device."
     sudo umount "$ventoy_mnt"
     continue
