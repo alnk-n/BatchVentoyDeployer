76874a0ae054f5a56a6ecfd362fa3858ef4fe817
alnk-n <alnnagorny@gmail.com>
2025-12-01 14:24:26 +0100

    feat: :sparkles: add initial download & setup, relative paths and change wording
    Refs #9

--- Ventoy-Installer-Script.sh
+++ Ventoy-Installer-Script.sh
@@ -7,8 +7,8 @@
 # To add a command alias, go into ~/.bashrc and under the #Aliases section add: alias clone-disks="sudo /path/to/your/Ventoy-Installer-Script.sh"
 
 # Variables:
-iso_src="/home/usbcs/ISOs"
-ventoy_script_src="/home/usbcs/ventoy-1.1.07/Ventoy2Disk.sh"
+iso_src="$HOME/ISOs"
+ventoy_script_src="./ventoy-1.1.07/Ventoy2Disk.sh"
 marker_file="./.init-setup-marker.txt"
 
 separator() {
@@ -22,52 +22,76 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 # Initial setup check
+# -------------------------------------------------------------------------
 if [ ! -f "$marker_file" ]; then
-  echo "Initial setup running..."
+  printf "Initial setup running..."
 
   apt update
   
-  if apt install -y zenity; then
-    echo "Zenity installed successfully."
+  # Create ISO source folder
+  if [ ! -d "$iso_src" ]; then
+  mkdir "$iso_src"
+  printf "- Created ISO source folder @: $iso_src"
+  printf "- Place your ISO files in this directory."
   else
-    echo "Failed to install zenity. Exiting."
+  printf "- ISO source folder exists @ $iso_src. Continuing."
+  fi
+
+  # Install Zenity
+  if apt install -y zenity; then
+    printf "- Zenity installed successfully."
+  else
+    printf "- Failed to install zenity. Exiting."
     exit 1
   fi
-  
+
+  # Check for Curl installation
+  if dpkg -l | grep -q "curl"; then
+    printf "- Curl is installed."
+  else
+    printf "- Curl is not installed. Downloading."
+        sudo apt-get update && sudo apt-get install curl
+  fi
+
+  # cURL Ventoy script and extract it from the archive
+  if curl -i https://github.com/ventoy/Ventoy/releases/download/v1.1.07/ventoy-1.1.07-linux.tar.gz -O; then
+    printf "- Ventoy tarball downloaded successfully."
+    tar -xvzf ./ventoy-1.1.07.tar.gz; rm -f ./ventoy-1.1.07.tar.gz
+    printf "- Ventoy tarball extracted successfully."   
+  else
+    printf "- Failed to download Ventoy tarball. Exiting."
+    exit 1
+  fi
+
   touch "$marker_file"
 fi
 
+# Main script
+# -------------------------------------------------------------------------
 printf '%*s\n' 60 | tr ' ' '\n'
 printf "=== Ventoy USB Creator ===\n\n"
 printf "This tool creates bootable Ventoy USB drives.\n"
 printf "WARNING: This process is irreversible. The selected disks will be erased.\n\n"
 
-# list disk names, model
+# List disk names, model, size
 printf "Detected disks:\n"
 separator
 lsblk --nodeps -o NAME,MODEL,SIZE | grep -v '^loop'
 separator
 echo
 
-# ask for disk choices
+# Ask for disk selection
 printf "(press Enter to exit)\n"
 printf "Enter the disks you wish to format, separated by spaces (e.g. sdd sde sdf):\n"
 read -p "> " choices
 
-# exit if no disks are selected
+# Exit if no disks are selected
 if [ -z "$choices" ]; then
   printf "No disks selected. Exiting."
   exit 1
 fi
 
-# check if ISO source folder exists
-if [ ! -d "$iso_src" ]; then
-  printf "ISO source folder not found: $iso_src"
-  printf "Create that directory and place your ISO files in there."
-  exit 1
-fi
-
-# main script
+# Copy ISOs to pendrives one by one
 for choice in $choices; do
   device="/dev/$choice"
 
@@ -78,16 +102,16 @@ for choice in $choices; do
   fi
 
   echo
-  printf "Installing Ventoy on $device...\n"
+  printf "Formatting $device with Ventoy...\n"
   sleep 1
 
-  # try ventoy script
+  # Try using Ventoy script
   "$ventoy_script_src" -I -s -g "$device" || {
     printf "Ventoy installation failed for $device. Skipping."
     continue
   }
 
-  # make dynamic mount point using selected disk input
+  # Create dynamic mount point using selected disk input
   ventoy_mnt="/mnt/ventoy_$choice"
   mkdir -p "$ventoy_mnt"
 
@@ -95,22 +119,22 @@ for choice in $choices; do
   printf "Mounting Ventoy data partition for $device...\n"
   sleep 1
 
-  # outputs path to main ventoy parition on specified drive
+  # Fetch path to main Ventoy parition on specified drive
   ventoy_part=$(lsblk -o NAME,LABEL -nr "$device" | awk '$2=="Ventoy"{print "/dev/" $1}')
 
-  # check for main ventoy partition
+  # Check for main Ventoy partition
   if [ -z "$ventoy_part" ]; then
     printf "Could not find Ventoy partition on $device. Skipping."
     continue
   fi
 
-  # try to mount Ventoy partition
+  # Try to mount main Ventoy partition
   mount "$ventoy_part" "$ventoy_mnt" || {
     printf "Failed to mount $ventoy_part to $ventoy_mnt. Skipping."
     continue
   }
 
-  # try to change permissions so anyone can write to the Ventoy partition
+  # Give anyone write permissions to the Ventoy partition
   chmod -R 777 "$ventoy_mnt" || {
     printf "Failed to change permissions (chmod couldn't change the permissions of the main partition). Skipping."
     continue
@@ -129,7 +153,7 @@ for choice in $choices; do
   echo
   printf "Unmounting $ventoy_mnt...\n"
 
-  # unmount ventoy partition and remove temporary mount point
+  # Unmount main Ventoy partition and delete temporary mount point
   umount "$ventoy_mnt"
   rmdir "$ventoy_mnt"
 
